c2ae7076928d2515622098b3d11ae9ea05e0e92a
Tech Rules <66055840+techrules07@users.noreply.github.com>
2025-12-09 11:40:03 +0530

    initial setup for payments

---
+++ src/test/java/com/qbatz/payment/QbatzPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qbatz.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QbatzPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
